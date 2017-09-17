e6b5e4eeba291bdf5fe6297c4ec8331df1e560e8
ojlee <dldhwn56@gmail.com>
2017-09-17 14:10:35 +0900

    abc

--- app/src/main/java/com/example/leeohju/becomeyoureyes/GuideActivity.java
+++ app/src/main/java/com/example/leeohju/becomeyoureyes/GuideActivity.java
@@ -1,7 +1,10 @@
 package com.example.leeohju.becomeyoureyes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GuideActivity extends AppCompatActivity {
 
@@ -9,6 +12,31 @@ public class GuideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_guide);
+
+        Button btnDes1 = (Button) findViewById(R.id.btndes1);
+        btnDes1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GuideActivity.this,CosActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnDes2 = (Button) findViewById(R.id.btndes2);
+        btnDes2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button btnDes3 = (Button) findViewById(R.id.btndes3);
+//        btnDes3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
     }
 }
